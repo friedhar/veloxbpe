fadd0f9cd423008d84e12a6b99a97607450949a2
friedhar <ronfrie@proton.me>
2025-02-20 20:42:52 +0200

    lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
+
+use bytepair::BytePair;
 pub mod bytepair;
-
-const BYTEPAIR_CAPACITY: usize = 8;
-
 pub struct Tokenizer {
     vocab: Vocab,
 }
 
 pub struct Vocab {
-    inner: HashMap<Vec<u8>, u64>,
-}
-
-pub struct BytePair {
-    inner: [u8; BYTEPAIR_CAPACITY],
-    length: usize,
-}
-
-impl BytePair {
-    pub fn from_slice(x: &[u8]) -> Option<BytePair> {
-        if x.len() > BYTEPAIR_CAPACITY {
-            return None;
-        }
-        let mut inner = [0; BYTEPAIR_CAPACITY];
-        for (ix, i) in x.into_iter().enumerate() {
-            inner[ix] = *i; // u8 is cheap to copy..
-        }
-
-        Some(BytePair {
-            inner,
-            length: x.len(),
-        })
-    }
-
-    pub fn compare(&self, x: &[u8]) -> bool {
-        if x.len() != self.length {
-            return false;
-        }
-
-        for ix in 0..x.len() {
-            if x[ix] != self.inner[ix] {
-                dbg!(&self.inner);
-                return false;
-            }
-        }
-        true
-    }
+    inner: BTreeMap<BytePair, u64>,
 }
 
 impl Tokenizer {
