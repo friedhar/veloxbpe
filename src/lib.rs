2993e73f3851c82686273ed28c7d8bd6d9444e0d
friedhar <ronfrie@proton.me>
2025-02-20 20:34:09 +0200

    initial no heap alloc BytePair impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,48 @@
+use std::collections::HashMap;
+
+const BYTEPAIR_CAPACITY: usize = 8;
+
 pub struct Tokenizer {
     vocab: Vocab,
 }
+
 pub struct Vocab {
-    inner: (),
+    inner: HashMap<Vec<u8>, u64>,
+}
+
+pub struct BytePair {
+    inner: [u8; BYTEPAIR_CAPACITY],
+    length: usize,
+}
+
+impl BytePair {
+    pub fn from_slice(x: &[u8]) -> Option<BytePair> {
+        if x.len() > BYTEPAIR_CAPACITY {
+            return None;
+        }
+        let mut inner = [0; BYTEPAIR_CAPACITY];
+        for (ix, i) in x.into_iter().enumerate() {
+            inner[ix] = *i; // u8 is cheap to copy..
+        }
+
+        Some(BytePair {
+            inner,
+            length: x.len(),
+        })
+    }
+
+    pub fn compare(&self, x: &[u8]) -> bool {
+        if x.len() != BYTEPAIR_CAPACITY {
+            return false;
+        }
+
+        for ix in 0..self.length {
+            if x[ix] != self.inner[ix] {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 impl Tokenizer {
