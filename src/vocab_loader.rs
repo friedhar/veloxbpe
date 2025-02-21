c6d2e0d41959fbd71a5100dc59c42170f039bab0
friedhar <ronfrie@proton.me>
2025-02-21 10:48:18 +0200

    mod VocabFetcher trait + more, incomplete, for now..

--- src/vocab_loader.rs
+++ src/vocab_loader.rs
@@ -5,23 +5,27 @@ use anyhow::Result;
 use crate::{bytepair::BytePair, vocab::Vocab};
 
 pub trait VocabFetcher {
-    fn load_raw(&self) -> String;
+    fn id() -> &'static str;
+    fn load_raw(&self) -> Result<String>;
+    fn parse(&self, x: String) -> Result<Vocab>;
 }
 
 pub struct O200kBase {}
 impl VocabFetcher for O200kBase {
-    fn load(&self) -> Vocab {
+    fn load_raw(&self) -> Result<String> {
         let resp = reqwest::blocking::ClientBuilder::new()
             .timeout(Duration::from_secs(1200))
             .build()
             .unwrap()
             .get("https://openaipublic.blob.core.windows.net/encodings/o200k_base.tiktoken")
-            .send()
-            .unwrap()
-            .text()
-            .unwrap();
+            .send()?
+            .text()?;
         println!("{}", resp.len());
-        BTreeMap::new()
+        Ok(resp)
+    }
+
+    fn parse(&self, x: String) -> Result<Vocab> {
+        Ok(BTreeMap::new())
     }
 }
 
@@ -41,6 +45,8 @@ pub const VOCAB_CACHE_DIR: &str = ".veloxbpe";
 impl<T: VocabFetcher> VocabLoader<T> {
     pub fn load(&self) -> Result<Vocab> {
         mkdir_if_needed(VOCAB_CACHE_DIR)?;
+        match 
+        let raw = self.x.load_raw()?;
         Ok(BTreeMap::new())
     }
 }
