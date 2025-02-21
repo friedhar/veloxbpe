4279cf1e6f8a23ac3bd7295c8c120e2e746cc659
friedhar <ronfrie@proton.me>
2025-02-21 14:02:15 +0200

    SmallString::new

--- src/smallstring.rs
+++ src/smallstring.rs
@@ -1,4 +1,22 @@
+const SMALLSTRING_CAPACITY: usize = 8;
+
 pub struct SmallString {
     inner: [char; 8],
     length: usize,
+}
+
+impl SmallString {
+    pub fn new(s: &str) -> SmallString {
+        if s.len() > SMALLSTRING_CAPACITY {
+            todo!("retrun Err()");
+        }
+        let mut inner = ['\0'; SMALLSTRING_CAPACITY];
+        for (ix, i) in s.chars().enumerate() {
+            inner[ix] = i;
+        }
+        SmallString {
+            inner,
+            length: s.len(),
+        }
+    }
 }
