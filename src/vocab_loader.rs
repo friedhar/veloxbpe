59433fce5f2e89ca17338d2493e6efcd99858b3e
friedhar <ronfrie@proton.me>
2025-02-21 10:14:09 +0200

    vocab_loader.rs

--- src/vocab_loader.rs
+++ src/vocab_loader.rs
@@ -5,3 +5,8 @@ use crate::{bytepair::BytePair, vocab::Vocab};
 pub trait VocabLoader {
     fn load(&self) -> Vocab;
 }
+
+pub struct O200kBase {}
+impl VocabLoader for O200kBase {
+    fn load(&self) -> Vocab {}
+}
