031fff05737b378cec9d16bab15b9e8ca01dbe50
friedhar <ronfrie@proton.me>
2025-02-21 21:23:37 +0200

    smallstring.rs

--- src/smallstring.rs
+++ src/smallstring.rs
@@ -1,67 +1,6 @@
-use core::panic;
 use std::hash::{Hash, Hasher};
 
-use serde::{Deserialize, Serialize};
-
-const SMALLSTRING_CAPACITY: usize = 128;
-
-#[derive(Ord, Eq, PartialEq, PartialOrd, Clone, Hash)]
-pub enum SmartString {
-    Stack(TinyString),
-    Heap(String),
-}
-
-impl SmartString {
-    pub fn new(s: &str) -> SmartString {
-        match s.len() > SMALLSTRING_CAPACITY {
-            true => SmartString::Heap(s.to_string()),
-            false => SmartString::Stack(TinyString::new(s)),
-        }
-    }
-
-    pub fn fuse(a: &SmartString, b: &SmartString) -> SmartString {
-        use SmartString::*;
-        match (a, b) {
-            (Stack(a), Stack(b)) => match a.length + b.length > SMALLSTRING_CAPACITY {
-                true => panic!(),
-
-                false => {
-                    let mut inner = [0; SMALLSTRING_CAPACITY];
-                    for (ix, i) in (&a.inner[..a.length]).into_iter().enumerate() {
-                        inner[ix] = *i;
-                    }
-
-                    for (ix, i) in (&b.inner[..b.length]).into_iter().enumerate() {
-                        inner[a.length + ix] = *i;
-                    }
-
-                    println!("noalloc");
-                    SmartString::Stack(TinyString {
-                        inner,
-                        length: a.length + b.length,
-                    })
-                }
-            },
-            _ => panic!(), // (Stack(a), Heap(b)) => SmartString::Heap(format!("{}{}", a.to_string(), b)),
-                           // (Heap(a), Stack(b)) => SmartString::Heap(format!("{}{}", a, b.to_string())),
-                           // (Heap(a), Heap(b)) => SmartString::Heap(format!("{}{}", a, b)),
-        }
-    }
-
-    pub fn from_char(c: char) -> SmartString {
-        SmartString::Stack(TinyString::new(c.to_string().as_str()))
-    }
-}
-
-impl ToString for SmartString {
-    fn to_string(&self) -> String {
-        use SmartString::*;
-        match self {
-            Stack(x) => x.to_string(),
-            Heap(x) => x.to_string(),
-        }
-    }
-}
+pub const SMALLSTRING_CAPACITY: usize = 128;
 
 #[derive(Ord, Eq, PartialEq, PartialOrd, Clone)]
 pub struct TinyString {
